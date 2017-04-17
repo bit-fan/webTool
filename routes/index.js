8a5af90ad02ab4c32ae39126aedc78fa55fb47a9
bit-fan <taur57@hotmail.com>
2017-04-17 12:35:02 +0800

    add index

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var func = require('../dbModule/util');
-const sysConfig = require('../config');
+const sysConfig = require('../config').config();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('home', {title: 'Express', ip: sysConfig.ip, port: sysConfig.port});
