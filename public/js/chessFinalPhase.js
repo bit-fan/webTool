6393a77fc7eb6807278361821c56a273f6eef669
asd <taur57@hotmail.com>
2017-10-10 09:39:09 +0800

    add blinking effect whet init board.

--- public/js/chessFinalPhase.js
+++ public/js/chessFinalPhase.js
@@ -23,6 +23,51 @@
             }
         }
 
+        function addDragEvt(domQuery, startEvt, nextFunc, finalFunc,errFunc) {
+
+            const mouseDown = Rx.Observable.create(function (o) {
+                $('body').on('mousedown', domQuery, function (ev) {
+                    ev.preventDefault();
+                    o.onNext(ev);
+                })
+            });
+
+            const mouseUp = Rx.Observable.create(function (o) {
+                $('body').on('mouseup', function (ev) {
+                    ev.preventDefault();
+                    console.log('mouseup');
+                    o.onNext(ev);
+                    finalFunc(ev);
+                })
+            });
+
+            const body = document.body;
+            // const mouseUp = Rx.Observable.fromEvent(body, 'mouseup');
+            const mouseMove = Rx.Observable.fromEvent(body, 'mousemove');
+
+            mouseDown
+                .map(event => {
+                    startEvt(event);
+                    return mouseMove.takeUntil(mouseUp)
+                })
+                .concatAll()
+                .map(event => {
+                    // console.log('event', event);
+                    // return {x: event.clientX, y: event.clientY}
+                    return event;
+                })
+                .subscribe(evt => {
+                    // console.log(dragDOM);
+                    // dragDOM[0].style.left = pos.x + 'px';
+                    // dragDOM[0].style.top = pos.y + 'px';
+                    nextFunc(evt)
+                }, err => {
+                    errFunc(err);
+                }, () => {
+                    console.log('done');
+                })
+        }
+
 
         function setBaseData(pieceFolder, pieceExt, boardTag, callback) {
             pieceFolder = pieceFolder || curPieceFolder;
@@ -55,44 +100,65 @@
             let colUnit = (100 - baseData.board.mLeft - baseData.board.mRight) / 8;
             let rowUnit = (100 - baseData.board.mTop - baseData.board.mBtm) / 9;
 
+            let avalPieceObj = {
+                bb: [],
+                bj: [],
+                bs: [],
+                bx: [],
+
+                rb: [],
+                rj: [],
+                rs: [],
+                rx: [],
+            };
             for (let row = 0; row < 10; row++) {
                 for (let col = 0; col < 9; col++) {
                     let avalPiece = [];
+                    let label = col + 1 + '' + (row + 1);
+
                     //红兵
                     if (row < 6 || (row === 6 && col % 2 === 0)) {
                         avalPiece.push('rb');
+                        avalPieceObj['rb'].push(label);
                     }
                     //黑兵
                     if (row > 3 || (row === 3 && col % 2 === 0)) {
                         avalPiece.push('bb');
+                        avalPieceObj['bb'].push(label);
                     }
                     //红士
                     let testRS = (row - 8) * (row - 8) + (col - 4) * (col - 4);
                     if (testRS === 0 || testRS === 2) {
                         avalPiece.push('rs');
+                        avalPieceObj['rs'].push(label);
                     }
                     if (testRS < 2.1) {
                         avalPiece.push('rj');
+                        avalPieceObj['rj'].push(label);
                     }
                     //红象
                     let testRX1 = (row - 7) * (row - 7) + (col - 2) * (col - 2);
                     let testRX2 = (row - 7) * (row - 7) + (col - 6) * (col - 6);
                     if (testRX1 === 4 || testRX2 === 4) {
                         avalPiece.push('rx');
+                        avalPieceObj['rx'].push(label);
                     }
                     //黑士
                     let testBS = (row - 1) * (row - 1) + (col - 4) * (col - 4);
                     if (testBS === 0 || testBS === 2) {
                         avalPiece.push('bs');
+                        avalPieceObj['bs'].push(label);
                     }
                     if (testBS < 2.1) {
                         avalPiece.push('bj');
+                        avalPieceObj['bj'].push(label);
                     }
                     //黑象
                     let testBX1 = (row - 2) * (row - 2) + (col - 2) * (col - 2);
                     let testBX2 = (row - 2) * (row - 2) + (col - 6) * (col - 6);
                     if (testBX1 === 4 || testBX2 === 4) {
                         avalPiece.push('bx');
+                        avalPieceObj['bx'].push(label);
                     }
 
                     baseData.boardMatrix[(col + 1) + '' + (row + 1)] = {
@@ -100,33 +166,79 @@
                         row: row + 1,
                         left: baseData.board.mLeft + col * colUnit,
                         top: baseData.board.mTop + row * rowUnit,
-                        availPieceArr: avalPiece
+                        availPieceArr: avalPiece,
+
                     }
+
                 }
             }
+            baseData.availPieceByPiece = avalPieceObj;
             console.log(baseData);
             if (callback) {
                 callback();
             }
         }
 
-        function initSetupPage() {
+        function initSetupBoard() {
             let row1 = $('<div>').attr('id', 'piecerow1');
             let row2 = $('<div>').attr('id', 'piecerow2');
             let width = $('#piecesListWrapper').width();
+            let finalBoard={};
 
             ['r', 'b'].forEach(color => {
                 let nowDiv = color === "r" ? row1 : row2;
-                ['c', 'm', 'p', 'b', 's', 'x'].forEach(piece => {
+                ['c', 'm', 'p', 'b', 's', 'x', 'j'].forEach(piece => {
                     let pieceDiv = $("<img>", {
                         src: baseData[color + piece].img,
                         class: "pieceSize pickPiece"
-                    });
+                    }).attr('pieceName', color + piece);
                     nowDiv.append(pieceDiv);
                 })
             })
             $('#piecesList').html('').append(row1).append(row2);
-            drawBoard();
+
+
+            $.each(baseData.boardMatrix, (k, v) => {
+                if (v.availPieceArr.length > 0) {
+                    console.log('v.col, v.row, v.availPieceArr', v.col, v.row, v.availPieceArr);
+                    let img = $('<img>', {
+                        width: 50,
+                        height: 50
+                    }).addClass('.pieceSize availPiece' + v.col + v.row);
+                    let newDiv = $('<div>')
+                        .attr('col', v.col)
+                        .attr('row', v.row)
+                        .addClass("pieceSize pieceImg piececandidate")
+                        .css('top', v.top + '%')
+                        .css('left', v.left + '%').append(img);
+                    $('#chessboarddiv').append(newDiv);
+                    // changePiece('.test' + v.col + v.row, v.col, v.row, v.availPieceArr, 0);
+                }
+            })
+            let piece = null,thisPieceDom=null,targetPos=null;
+            addDragEvt('.pickPiece', function (evt) {
+                thisPieceDom=$(evt.target);
+                piece = $(evt.target).attr('pieceName');
+                $('#chessboarddiv .blink').removeClass('blink');
+                if (baseData.availPieceByPiece[piece]) {
+                    baseData.availPieceByPiece[piece].forEach(label => {
+                        $('#chessboarddiv .availPiece' + label).prop('src', baseData[piece].img).addClass('blink');
+                    })
+                } else {
+                    //blink all
+                }
+                console.log('click', evt)
+            }, function (evt) {
+                console.log('move', evt);
+                let a=document.elementFromPoint(evt.clientX-25, evt.clientY-25)
+                let className=evt.target.className;
+                console.log(className,a);
+                $(thisPieceDom).offset({top:evt.clientY-25,left:evt.clientX-25});
+            },function(ev){
+                console.log('final',ev);
+            }, function (evt) {
+                console.log('err', evt)
+            });
 
         }
 
@@ -136,7 +248,7 @@
             $('#chessboarddiv').height(width * baseData.board.height / baseData.board.width);
 
             // testPos(baseData.board.mTop, baseData.board.mBtm, baseData.board.mLeft, baseData.board.mRight);
-            testPiece();
+            // testPiece();
         }
 
         function testPos(mTop, mBtm, mLeft, mRight) {
@@ -160,7 +272,7 @@
         function testPiece() {
             $('.testImg').remove();
             function changePiece(className, col, row, arr, idx) {
-                console.log('col,row,arr,idx', className,col, row, arr, idx,baseData[arr[idx]].img);
+                console.log('col,row,arr,idx', className, col, row, arr, idx, baseData[arr[idx]].img);
                 $(className).prop('src', baseData[arr[idx]].img);
                 setTimeout(function () {
                     changePiece(className, col, row, arr, (idx + 1) % arr.length)
@@ -171,9 +283,9 @@
                 if (v.availPieceArr.length > 0) {
                     console.log('v.col, v.row, v.availPieceArr', v.col, v.row, v.availPieceArr);
                     let className = "pieceSize testImg";// test" + v.col + v.row;
-                    let img=$('<img>',{
-                        width:50,
-                        height:50
+                    let img = $('<img>', {
+                        width: 50,
+                        height: 50
                     }).addClass('.pieceSize test' + v.col + v.row);
                     let newDiv = $('<div>')
                         .addClass("pieceSize testImg")
@@ -214,7 +326,7 @@
                     console.log('board change');
                     curBoardTag = $('select#boardType').val();
                     setBaseData(null, null, curBoardTag, function () {
-                        initSetupPage();
+                        drawBoard();
                     })
                 });
                 $('select#pieceType').on('change', target => {
@@ -223,8 +335,11 @@
                     curPieceExt = tag[1];
                     console.log('piece change');
                     setBaseData(curPieceFolder, curPieceExt, null, function () {
-                        initSetupPage();
+                        drawBoard();
                     })
+                });
+                $('#btnSetupBoard').on('click', () => {
+                    initSetupBoard();
                 });
                 $('#confirm').on('click', function () {
                     testPos(parseInt($('#top').val()), parseInt($('#btm').val()), parseInt($('#left').val()), parseInt($('#right').val()));
