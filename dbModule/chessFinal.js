1e2cc4f4398ae250ff079c24d342ced4ccee645a
asd <taur57@hotmail.com>
2017-10-14 00:50:38 +0800

    castle movement

--- dbModule/chessFinal.js
+++ dbModule/chessFinal.js
@@ -40,7 +40,7 @@ var local = {
             return {valid: false, data: {}};
         }
         return {
-            valid: true, data: {
+            valid: true, posArr1: {
                 bb: [pos[6], pos[7], pos[8], pos[9], pos[10]],
                 bc: [pos[0], pos[1]],
                 bj: [pos[11]],
@@ -56,7 +56,7 @@ var local = {
                 rp: [pos[20], pos[21]],
                 rs: [pos[28], pos[29]],
                 rx: [pos[30], pos[31]]
-            }
+            }, posArr: pos
         };
     },
     parseBoardToStr(obj){
@@ -94,30 +94,99 @@ var local = {
             return {valid: false, str: ''}
         }
     },
-    getHorseNextPos
-        (board, curPos)
-    {
+    getPos(type, cur, x, y, para1, para2){
+        if (type == 'offset') {
+            x += parseInt(cur[0]);
+            y += parseInt(cur[1]);
+            if (x < 1 || x > 9 || y < 0 || y > 9) {
+                return false;
+            } else {
+                return '' + x + y;
+            }
+        } else if (type == "march") {
+            let item = local.getPos("offset", cur, x, y);
+            if (item) {
+                return getPos("march", item, x, y).push(item);
+            } else {
+                return [];
+            }
+        }
+    },
+    boardUtil(type, arr, cur, x, y){
+        if (type == "isExist") {
+            return arr.indexOf(cur) > -1;
+        } else if (type == "side") {
+            let idx = arr.indexOf(cur);
+            if (idx == -1) {
+                return null;
+            } else {
+                return idx < 16 ? 'r' : 'b';
+            }
+        } else if (type == "between") {
+            let x1 = parseInt(cur / 10), y1 = cur % 10, x2 = parseInt(x / 10), y2 = x % 10;
+            let resultArr = [];
+            if (x1 != x2 && y1 != y2) {
+                return [];
+            } else if (x1 == x2) {
+                for (let i = Math.min(y1, y2); i < Math.max(y1, y2); i++) {
+                    resultArr.push('' + x1 + i);
+                }
+                return resultArr;
+            } else if (y1 == y2) {
+                for (let i = Math.min(x1, x2); i < Math.max(x1, x2); i++) {
+                    resultArr.push('' + i + x1);
+                }
+                return resultArr;
+            }
+        }
+    },
+    getCastleNextPos(pieceArr, curPos){
+        let side = pieceArr.indexOf(curPos) < 16 ? 'r' : 'b';
+        let nextArr = [];
+        let curX = parseInt(curPos / 10), curY = curPos % 10;
+        for (let i = 0; i < 10; i++) {
+            if (i != 0) {
+                nextArr.push(('' + curX + i));
+                nextArr.push(('' + i + curY));
+            } else {
+                nextArr.push(curX + '0');
+            }
+        }
+        console.log('pre next', nextArr);
+        return nextArr.filter(item => {
+            if (item == curPos) {
+                return false
+            }
+            if (local.boardUtil('between', pieceArr, curPos, item).length > 0) {
+                return false;
+            }
+            let testSide = local.boardUtil('isExist', pieceArr, item);
+            if (testSide && testSide == side) {
+                return false;
+            }
+            return true;
+        })
+    },
+
+    getHorseNextPos(pieceArr, curPos){
         let cC = curPos % 10, cR = parseInt(curPos / 10);
         let finalArr = [];
-        if (!board.matrix[cC - 1 + '' + cR]) {
-            finalArr.push([cC - 2, cR - 1]).push([cC - 2, cR + 1]);
+        if (!board.matrix[local.getPos('offset', curPos, -1, 0)]) {
+            finalArr.push(local.getPos('offset', curPos, -2, -1)).push(local.getPos('offset', curPos, -2, 1));
         }
-        if (!board.matrix[cC + 1 + '' + cR]) {
+        if (!board.matrix[local.getPos('offset', curPos, 1, 0)]) {
             finalArr.push([cC + 2, cR - 1]).push([cC + 2, cR + 1])
         }
-        if (!board.matrix[cC + '' + (cR - 1)]) {
+        if (!board.matrix[local.getPos('offset', curPos, 0, -1)]) {
             finalArr.push([cC - 1, cR - 2]).push([cC + 1, cR - 2])
         }
-        if (!board.matrix[cC + '' + (cR + 1)]) {
+        if (!board.matrix[local.getPos('offset', curPos, 0, 1)]) {
             finalArr.push([cC - 1, cR + 2]).push([cC + 1, cR + 2])
         }
         return finalArr.filter(arr => {
-            return arr[0] > 0 && arr[0] < 10 && arr[1] > -1 && arr[1] < 10;
-        }).map(arr => {
-            return arr[0] * 10 + arr[1]
+            return arr != false;
         });
-    }
-    ,
+    },
     getBishopNextPos(board, curPos)
     {
         let cC = curPos % 10, cR = parseInt(curPos / 10);
@@ -142,8 +211,7 @@ var local = {
         }).map(arr => {
             return arr[0] * 10 + arr[1]
         });
-    }
-    ,
+    },
     parseBoard(srcObj, step)
     {
         let board = {matrix: {}, piece: {}, step: step, status: ''};
@@ -155,21 +223,76 @@ var local = {
             board.matrix[pos] = pieceName;
         }
 
-    }
-    ,
-    isRedChecking(board)
+    },
+    getSolution(solObj, step){
+        console.log('solObj', solObj);
+        let found = false;
+        for (let boardKey in solObj) {
+            let nextboardArr = [];
+            if (solObj[boardKey].step == step) {
+                found = true;
+                if (!solObj[boardKey].status) {
+                    solObj[boardKey].posArr = local.parseStrToBoard(boardKey).posArr;
+                    solObj[boardKey].status = 'hasNext';
+                }
+                if (step % 2 == 0) {
+                    nextboardArr = local.getAllNextCheckingBoard(solObj, boardKey);
+                } else {
+                    nextboardArr = local.getAllNextEscapingBoard(solObj, boardKey);
+                }
+                nextboardArr.forEach(board => {
+                    solObj[board.boardKey] = {
+                        piece: board.piece,
+                        from: boardKey,
+                        stepId: step + 1,
+                        status: board.status
+                    }
+                });
+                if (nextboardArr.length == 0) {
+                    solObj[boardKey].status = "dead";
+                } else {
+                    solObj[boardKey].nextKeyArr = nextboardArr.map(item => item.boardKey);
+                }
+
+            }
+        }
+        if (found) {
+            return local.getSolution(solObj, step + 1)
+        } else {
+            return local.minifySol(solObj, step);
+        }
+
+    },
+    minifySol(solObj, curStep){
+        if (curStep == 0) {
+            return solObj;
+        } else {
+            //simpl
+            return local.minifySol(solObj, curStep - 1);
+        }
+    },
+    isRedChecking(board, piecesObj)
     {
 
-    }
-    ,
-    isBlackChecking(board)
+    },
+    isBlackChecking(board, piecesObj)
     {
 
-    }
-    ,
-    getAllNextCheckingBoard(board)
+    },
+    getAllNextCheckingBoard(solObj, boardKey)
     {
+        let posArr = solObj[boardKey].posArr;
+        if (posArr[0] != 0) {
+            console.log('next catle', local.getCastleNextPos(posArr, posArr[0]));
+        }
+        if (posArr[1] != 0) {
+            console.log(local.getCastleNextPos(posArr, posArr[1]));
+        }
+
         return [];
+    },
+    getAllNextEscapingBoard(boardKey, piecesObj){
+
     }
 }
 var socket = {
@@ -203,13 +326,25 @@ var socket = {
         }
         console.log('parse', local.parseBoardToStr(parseObj));
 
-        return {result: result, data: errorObj};
+        return {
+            result: result,
+            data: errorObj,
+            parseStr: result ? local.parseBoardToStr(parseObj).str : ''
+        };
     },
     chessStartBoard: function (req) {
         let validBoard = socket.chessValidateBoard(req);
         if (!validBoard.result) {
             return {result: false, data: [], text: "invalid board"};
         }
+        let solObj = {};
+        solObj[validBoard.parseStr] = {
+            nextBoardArr: [],
+            step: 0,
+            from: null,
+            result: 'working',//'done','nosolution'
+        }
+        local.getSolution(solObj, 0);
 
 
     }
